13cf4ce94146502b3360ec765a25d8570e39141d
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-03 20:17:13 +0000

    solution as subquery

--- main.sql
+++ main.sql
@@ -2,4 +2,4 @@
 Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 */
 
-select code from country where continent='Asia'
+select sum(population) from city where countrycode in (select code from country where continent='Asia')
