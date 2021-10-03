e7ecbf2ca22d6f1b3234fa4e5d7cdb49f5eba8f3
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-03 20:19:39 +0000

    solution as join

--- main.sql
+++ main.sql
@@ -2,4 +2,4 @@
 Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 */
 
-select sum(population) from city where countrycode in (select code from country where continent='Asia')
+select sum(CITY.population) from city inner join country on CITY.CountryCode=COUNTRY.Code where continent='Asia'
